5a4d16b339fa697504732d61f1decc8443c64d6b
Rob Blackbourn <rob.blackbourn@googlemail.com>
2018-12-15 16:44:29 +0000

    Added another vector test

--- example1/serialization/tests/test_vector.cpp
+++ example1/serialization/tests/test_vector.cpp
@@ -13,6 +13,7 @@ int main()
 
     ok = ok && test_value(std::vector<std::string>());
     ok = ok && test_value(std::vector<std::string>{std::string("Hello"), std::string("World")});
+    ok = ok && test_value(std::vector<int>{1, 2, 3, 4});
 
     return ok ? 0 : 1;
 }
