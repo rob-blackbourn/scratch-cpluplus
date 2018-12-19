1489516633d260575690012015d86ca2cd462e79
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2018-12-19 08:16:01 +0000

    Added a test for MulticastData

--- example1/messages/src/messages/MulticastData.hpp
+++ example1/messages/src/messages/MulticastData.hpp
@@ -14,40 +14,53 @@ namespace jetblack::messagebus::messages
   class MulticastData : public Message
   {
     public:
-      MulticastData() noexcept
-        : Message(MessageType::MulticastData)
-      {
-      }
+        MulticastData() noexcept
+            :   Message(MessageType::MulticastData)
+        {
+        }
 
-      MulticastData(
-        const std::string &feed,
-        const std::string &topic,
-        bool isImage,
-        std::vector<BinaryDataPacket> &&data) noexcept
-          : Message(MessageType::MulticastData),
-            _feed(feed),
-            _topic(topic),
-            _isImage(isImage),
-            _data(std::move(data))
-      {
-      }
+        MulticastData(
+            const std::string &feed,
+            const std::string &topic,
+            bool isImage,
+            std::vector<BinaryDataPacket> &&data) noexcept
+            :   Message(MessageType::MulticastData),
+                _feed(feed),
+                _topic(topic),
+                _isImage(isImage),
+                _data(std::move(data))
+        {
+        }
 
-      const std::string &feed() const noexcept { return _feed; }
-      const std::string &topic() const noexcept { return _topic; }
-      const bool isImage() const noexcept { return _isImage; }
-      const std::vector<BinaryDataPacket> &data() const noexcept { return _data; }
+        const std::string &feed() const noexcept { return _feed; }
+        const std::string &topic() const noexcept { return _topic; }
+        const bool isImage() const noexcept { return _isImage; }
+        const std::vector<BinaryDataPacket> &data() const noexcept { return _data; }
 
-      static std::shared_ptr<MulticastData> from_bytes(std::vector<char>::const_iterator &iter);
+        static std::shared_ptr<MulticastData> from_bytes(std::vector<char>::const_iterator &iter);
 
+        friend bool operator == (const MulticastData& lhs, const MulticastData& rhs) noexcept
+        {
+            return lhs._feed == rhs._feed &&
+                lhs._topic == rhs._topic &&
+                lhs._isImage == rhs._isImage &&
+                lhs._data == rhs._data;
+        }
+
+        friend bool operator != (const MulticastData& lhs, const MulticastData& rhs) noexcept
+        {
+            return !(lhs == rhs);
+        }
+        
     protected:
-      virtual size_t bodySize() const noexcept;
-      virtual void writeBody(std::vector<char>::iterator &sink) const;
+        virtual size_t bodySize() const noexcept;
+        virtual void writeBody(std::vector<char>::iterator &sink) const;
 
     private:
-      std::string _feed;
-      std::string _topic;
-      bool _isImage;
-      std::vector<BinaryDataPacket> _data;
+        std::string _feed;
+        std::string _topic;
+        bool _isImage;
+        std::vector<BinaryDataPacket> _data;
   };
 }
 
---
+++ example1/messages/tests/test_MulticastData.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <array>
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+
+#include "serialization/native.hpp"
+#include "serialization/string.hpp"
+
+#include "messages/MulticastData.hpp"
+#include "messages/BinaryDataPacket.hpp"
+
+using jetblack::messagebus::messages::MulticastData;
+using jetblack::messagebus::messages::Message;
+using jetblack::messagebus::messages::BinaryDataPacket;
+
+bool ShouldRoundTripSimple()
+{
+    std::string feed = "LSE";
+    std::string topic = "VOD";
+    bool isImage = true;
+
+    auto generator = boost::uuids::random_generator();
+    boost::uuids::uuid header(generator());
+    std::vector<char> body{'a', 'b', 'c'};
+    BinaryDataPacket packet{header, std::move(body)};
+    std::vector<BinaryDataPacket> data;
+    data.push_back(std::move(packet));
+
+    MulticastData value(feed, topic, isImage, std::move(data));
+
+    auto buf(value.to_bytes());
+    std::vector<char>::const_iterator iter = buf->begin();
+    auto roundTrip = std::static_pointer_cast<MulticastData>(Message::from_bytes(iter));
+    return value == *roundTrip;
+}
+
+int main()
+{
+    bool ok = true;
+
+    ok = ok && ShouldRoundTripSimple();
+
+    return ok ? 0 : 1;
+}
